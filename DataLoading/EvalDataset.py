39736f613e91f47e843be6294f95d3d1c17fa237
Chingis Oinar <chingisoinar@gmail.com>
2021-11-14 13:19:41 +0900

    Add files via upload

--- DataLoading/EvalDataset.py
+++ DataLoading/EvalDataset.py
@@ -28,21 +28,24 @@ import random
 
 
 class EvalDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None  ):
-        
-       
-        
+    def __init__(self, csv_file, root_dir, transform=None, optical_flow=True, public=False, private=False):
+        assert False in (public, private)
+        mode = -1
+        if public:
+            mode = 1
+        elif private:
+            mode = 0
         camera_csv = pd.read_csv(csv_file)
 
-        
+        self.optical_flow = optical_flow
         csv_len = len(camera_csv)
-
-        self.camera_csv = camera_csv[camera_csv['public'] == 1]
-            
+        camera_csv['og_idx'] = range(len(camera_csv))
+        self.camera_csv = camera_csv if mode == -1 else camera_csv[camera_csv['public'] == mode]
+        self.original_camera = camera_csv
         self.root_dir = root_dir
         self.transform = transform
-        
-
+        self.to_tensor = transforms.ToTensor()
+        self.normalize = transforms.Normalize(mean=0.5, std=0.5)
     
     def __len__(self):
         return len(self.camera_csv)
@@ -59,11 +62,37 @@ class EvalDataset(Dataset):
         angle = self.camera_csv['steering_angle'].iloc[idx]
         if self.transform:
             image_transformed = self.transform(image)
+        angle_t = torch.tensor(angle)
+        idx_t = torch.tensor(idx)
+        if self.optical_flow:
+            og_idx = self.camera_csv['og_idx'].iloc[idx]
+            if og_idx != 0:
+                
+                path = os.path.join(self.root_dir, str(self.original_camera['frame_id'].iloc[og_idx - 1]) + '.jpg')
+                prev = cv2.imread(path)
+                prev = cv2.cvtColor(prev, cv2.COLOR_BGR2RGB)
+                prev = prev[65:-25,:,:]
+                prev = cv2.cvtColor(prev, cv2.COLOR_RGB2GRAY)
+            else:
+                prev = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            cur = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            # Use Hue, Saturation, Value colour model
+            flow = cv2.calcOpticalFlowFarneback(prev, cur, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            hsv = np.zeros(image.shape, dtype=np.uint8)
+            hsv[..., 1] = 255
+            
+            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+            hsv[..., 0] = ang * 180 / np.pi / 2
+            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+            optical_rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            optical_rgb = self.normalize(self.to_tensor(optical_rgb))
+            del image
+            return image_transformed, angle_t, idx_t, optical_rgb
+               
+        if self.transform:
             del image
             image = image_transformed
-        angle_t = torch.tensor(angle)
-
-        return image, angle_t
+        return image, angle_t, idx_t
     
     def read_data(self, idx):
         if isinstance(idx, list):
@@ -75,8 +104,8 @@ class EvalDataset(Dataset):
                 for i, d in enumerate(new_data):
                     data[i].append(new_data[i])
                 del new_data
-                
-            for stack_idx in [0, 1]: # we don't stack timestamp and frame_id since those are string data
+            indeces = [0, 1, 2] if not self.optical_flow else [0, 1, 2, 3]
+            for stack_idx in indeces: # we don't stack timestamp and frame_id since those are string data
                 data[stack_idx] = torch.stack(data[stack_idx])
             
             return data
@@ -91,8 +120,10 @@ class EvalDataset(Dataset):
         data = self.read_data(idx)
         
         sample = {'image': data[0],
-                  'angle': data[1]}
-        
+                  'angle': data[1],
+                    'idx':data[2]}
+        if self.optical_flow:
+            sample['optical'] = data[3]
         del data
         
         return sample
--- DataLoading/UdacityDataset.py
+++ DataLoading/UdacityDataset.py
@@ -20,7 +20,7 @@ import random
 
 
 class UdacityDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None, select_camera=None, slice_frames=None, select_ratio=1.0, select_range=None):
+    def __init__(self, csv_file, root_dir, transform=None, select_camera=None, slice_frames=None, select_ratio=1.0, select_range=None, optical_flow=True):
         
         assert select_ratio >= -1.0 and select_ratio <= 1.0 # positive: select to ratio from beginning, negative: select to ration counting from the end
         
@@ -52,14 +52,15 @@ class UdacityDataset(Dataset):
             
         self.root_dir = root_dir
         self.transform = transform
-        
+        self.optical_flow = optical_flow
         # Keep track of mean and cov value in each channel
         self.mean = {}
         self.std = {}
         for key in ['angle', 'torque', 'speed']:
             self.mean[key] = np.mean(camera_csv[key])
             self.std[key] = np.std(camera_csv[key])
-    
+        self.to_tensor = transforms.ToTensor()
+        self.normalize = transforms.Normalize(mean=0.5, std=0.5)
     def __len__(self):
         return len(self.camera_csv)
     
@@ -68,21 +69,47 @@ class UdacityDataset(Dataset):
         image = cv2.imread(path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = image[65:-25,:,:]
+        
         timestamp = self.camera_csv['timestamp'].iloc[idx]
         frame_id = self.camera_csv['frame_id'].iloc[idx]
         angle = self.camera_csv['angle'].iloc[idx]
         torque = self.camera_csv['torque'].iloc[idx]
         speed = self.camera_csv['speed'].iloc[idx]
-
-        if self.transform:
-            image_transformed = self.transform(image)
-            del image
-            image = image_transformed
-        angle_t = torch.tensor(angle)
+        angle_t = torch.tensor(angle) * 50
         torque_t = torch.tensor(torque)
         speed_t = torch.tensor(speed)
-        del angle, torque, speed
+  
+        if self.transform:
+            image_transformed = self.transform(image)
+
+        if self.optical_flow:
+            if idx != 0:
+                path = os.path.join(self.root_dir, self.camera_csv['filename'].iloc[idx - 1])
+                prev = cv2.imread(path)
+                prev = cv2.cvtColor(prev, cv2.COLOR_BGR2RGB)
+                prev = prev[65:-25,:,:]
+                prev = cv2.cvtColor(prev, cv2.COLOR_RGB2GRAY)
+            else:
+                prev = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            cur = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            # Use Hue, Saturation, Value colour model
+            flow = cv2.calcOpticalFlowFarneback(prev, cur, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            hsv = np.zeros(image.shape, dtype=np.uint8)
+            hsv[..., 1] = 255
             
+            mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+            hsv[..., 0] = ang * 180 / np.pi / 2
+            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+            optical_rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            optical_rgb = self.normalize(self.to_tensor(optical_rgb))
+            del image
+            return image_transformed, timestamp, frame_id, angle_t, torque_t, speed_t, optical_rgb
+        
+        if self.transform:
+            del image
+            image = image_transformed
+        
+        
         return image, timestamp, frame_id, angle_t, torque_t, speed_t
     
     def read_data(self, idx):
@@ -95,9 +122,12 @@ class UdacityDataset(Dataset):
                 for i, d in enumerate(new_data):
                     data[i].append(new_data[i])
                 del new_data
-                
-            for stack_idx in [0, 3, 4, 5]: # we don't stack timestamp and frame_id since those are string data
-                data[stack_idx] = torch.stack(data[stack_idx])
+            if self.optical_flow:
+                for stack_idx in [0, 3, 4, 5, 6]: # we don't stack timestamp and frame_id since those are string data
+                    data[stack_idx] = torch.stack(data[stack_idx])
+            else:
+                for stack_idx in [0, 3, 4, 5]: # we don't stack timestamp and frame_id since those are string data
+                    data[stack_idx] = torch.stack(data[stack_idx])
             
             return data
         
@@ -116,6 +146,8 @@ class UdacityDataset(Dataset):
                   'angle': data[3],
                   'torque': data[4],
                   'speed': data[5]}
+        if self.optical_flow:
+            sample['optical'] = data[6]
         
         del data
         
