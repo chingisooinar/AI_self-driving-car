d1b6bdd49d32dab51421ece04637aa9ab1e7fdf7
eunmin88 <eunmin88@gmail.com>
2021-11-14 20:34:58 +0900

    Changes

--- transfer_train.py
+++ transfer_train.py
@@ -23,7 +23,6 @@ from DataLoading import ConsecutiveBatchSampler as CB
 import random
 from model.TransferLearning import TLearning
 from torch.nn.parallel import DistributedDataParallel as DDP
-import wandb
 from DataLoading.aug_utils import change_image_brightness_rgb, add_random_shadow
 # noinspection PyAttributeOutsideInit
 class AverageMeter(object):
@@ -43,46 +42,49 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
 device = torch.device("cuda")
 
 parameters = edict(
     learning_rate = 0.001,
     batch_size = 32,
     seq_len = 5,
-    num_workers = 8,
+    num_workers = 4,
 )
 
 
-network = TLearning()
-network = torch.nn.DataParallel(network).to(device)
+network = TLearning().to(device)
 
-wandb.init(config=parameters, project='self-driving-car')
-wandb.watch(network)
 optimizer = optim.Adam(network.parameters(),lr = parameters.learning_rate,betas=(0.9, 0.999), eps=1e-08, weight_decay=0.001)
 
-udacity_dataset = UD.UdacityDataset(csv_file='/home/chingis/self-driving-car/output/interpolated.csv',
-                             root_dir='/home/chingis/self-driving-car/output/',
+udacity_dataset = UD.UdacityDataset(csv_file='C:\\users/rladm/Desktop/training_data/output/interpolated.csv',
+                             root_dir='C:\\Users/rladm/Desktop/training_data/output/',
                              transform=transforms.Compose([transforms.ToTensor()]),
                              select_camera='center_camera')
 
 dataset_size = int(len(udacity_dataset))
 del udacity_dataset
+
 split_point = int(dataset_size * 0.9)
 
-training_set = UD.UdacityDataset(csv_file='/home/chingis/self-driving-car/output/interpolated.csv',
-                             root_dir='/home/chingis/self-driving-car/output/',
+training_set = UD.UdacityDataset(csv_file='C:\\/Users/rladm/Desktop/training_data/output/interpolated.csv',
+                             root_dir='C:\\Users/rladm/Desktop/training_data/output/',
                              optical_flow=False,
                              transform=transforms.Compose([
+                                 transforms.Lambda(lambda x: Image.fromarray(np.uint8(x)).convert('RGB')),
+                                 transforms.Resize([int(224), int(224)]),
                                  transforms.ToTensor(),
-                                 transforms.Normalize(0.5, 0.5)
+                                 transforms.Normalize(0.5, 0.5),
                                  ]),
                              select_camera='center_camera',
                              select_range=(0,split_point))
-validation_set = UD.UdacityDataset(csv_file='/home/chingis/self-driving-car/output/interpolated.csv',
-                             root_dir='/home/chingis/self-driving-car/output/',
+validation_set = UD.UdacityDataset(csv_file='C:\\Users/rladm/Desktop/training_data/output/interpolated.csv',
+                             root_dir='C:\\Users/rladm/Desktop/training_data/output/',
                              optical_flow=False,
-                             transform=transforms.Compose([transforms.ToTensor(),
-                                                           transforms.Normalize(0.5, 0.5)]),
+                             transform=transforms.Compose([transforms.Lambda(lambda x: Image.fromarray(np.uint8(x)).convert('RGB')),
+                             transforms.Resize([int(224), int(224)]),
+                             transforms.ToTensor(),
+                             transforms.Normalize(0.5, 0.5)]),            
                              select_camera='center_camera',
                              select_range=(split_point,dataset_size))
 
@@ -90,56 +92,60 @@ training_loader = DataLoader(training_set, shuffle=True, num_workers=parameters.
 validation_loader = DataLoader(validation_set,  shuffle=False, num_workers=parameters.num_workers, batch_size=parameters.batch_size)
 criterion =  torch.nn.MSELoss()
 criterion.to(device)
-for epoch in range(32):
-    losses = AverageMeter()
-    torch.save(network.state_dict(), "saved_models/TLearning/epoch-{}.tar".format(epoch))
-    network.train()
-    # Calculation on Training Loss
-    for training_sample in tqdm(training_loader):
-        training_sample['image'] = training_sample['image'].squeeze()
-        training_sample['image'] = torch.Tensor(resize(training_sample['image'], (parameters.batch_size,3,224,224),anti_aliasing=True))
 
-        
-        param_values = [v for v in training_sample.values()]
-        image, angle = param_values[0], param_values[3]
-
-
-        image = image.to(device)
-        prediction = network(image)
-
-        prediction = prediction.reshape(parameters.batch_size,1)
-        labels = angle.float().reshape(parameters.batch_size,1).to(device)
-
-        if labels.shape[0]!=prediction.shape[0]:
-            prediction = prediction[-labels.shape[0],:]
-
-        training_loss_angle =criterion(prediction,labels)
-
-        losses.update(training_loss_angle.item())
-        optimizer.zero_grad()# zero the gradient that are being held in the Grad attribute of the weights
-        training_loss_angle.backward() # calculate the gradients
-        optimizer.step() # finishing calculation on gradient
-    
-
-    network.eval()
-    # Calculation on Validation Loss
-    val_losses = AverageMeter()
-    with torch.no_grad():    
-        for Validation_sample in tqdm(validation_loader):
-            Validation_sample['image'] = Validation_sample['image'].squeeze()
-            Validation_sample['image'] = torch.Tensor(resize(Validation_sample['image'], (parameters.batch_size,3,224,224),anti_aliasing=True))
-
+if __name__ == '__main__':
+    torch.multiprocessing.freeze_support()
+    for epoch in range(32):
+        losses = AverageMeter()
+        torch.save(network.state_dict(), "saved_models/TLearning/epoch-{}.tar".format(epoch))
+        network.train()
+        # Calculation on Training Loss
+        for training_sample in tqdm(training_loader):
 
             param_values = [v for v in training_sample.values()]
             image, angle = param_values[0], param_values[3]
+
             image = image.to(device)
             prediction = network(image)
-            prediction = prediction.reshape(parameters.batch_size,1)
-            labels = angle.float().reshape(parameters.batch_size,1).to(device)
-            del param_values, image, angle
+
+            prediction = prediction.reshape(-1,1)
+            labels = angle.float().reshape(-1,1).to(device)
+
             if labels.shape[0]!=prediction.shape[0]:
-                prediction = prediction[-labels.shape[0],:]
-            validation_loss_angle = criterion(prediction,labels)
-            val_losses.update(validation_loss_angle.item())
-    wandb.log({'training_loss': losses.avg, 'val_loss': val_losses.avg})
+                prediction = prediction[-labels.shape[0],:].reshape(-1,1)
+
+            try:
+                assert labels.shape == prediction.shape
+            except AssertionError:
+                print('Failed')
+                raise
+
+            training_loss_angle =criterion(prediction,labels)
+
+            losses.update(training_loss_angle.item())
+            optimizer.zero_grad()# zero the gradient that are being held in the Grad attribute of the weights
+            training_loss_angle.backward() # calculate the gradients
+            optimizer.step() # finishing calculation on gradient
+        
+
+        network.eval()
+        # Calculation on Validation Loss
+        val_losses = AverageMeter()
+        with torch.no_grad():    
+            for Validation_sample in tqdm(validation_loader):
+
+                param_values = [v for v in training_sample.values()]
+                image, angle = param_values[0], param_values[3]
+                image = image.to(device)
+                prediction = network(image)
+                prediction = prediction.reshape(-1,1)
+                labels = angle.float().reshape(-1,1).to(device)
+                del param_values, image, angle
+                if labels.shape[0]!=prediction.shape[0]:
+                    prediction = prediction[-labels.shape[0],:]
+                validation_loss_angle = criterion(prediction,labels)
+                val_losses.update(validation_loss_angle.item())
+        print('training_loss: %d, val_loss: %d' % (losses.avg, val_losses.avg))
     
+
+
