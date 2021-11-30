4226f1defd9899ebf5ef3dc27ff8f466ecafe07b
Chingis Oinar <chingisoinar@gmail.com>
2021-11-30 14:41:56 +0900

    Add files via upload

--- exp_train.py
+++ exp_train.py
@@ -5,24 +5,20 @@ Created on Sat Nov  6 12:24:40 2021
 
 @author: chingis
 """
-from skimage.transform import resize
-from itertools import product
+
 from easydict import EasyDict as edict
 from tqdm import tqdm
-import torch.backends.cudnn as cudnn
+
 import torch
 from torch.utils.data import DataLoader
-from torchvision import transforms, utils, models
-import torch.nn.functional as F
+from torchvision import transforms
 import torch.optim as optim
-import numpy as np
 from DataLoading import UdacityDataset as UD
 from DataLoading import ConsecutiveBatchSampler as CB
-import random
-from model.TransMotion import Convolution3D
-from torch.nn.parallel import DistributedDataParallel as DDP
+from model.MotionTransformer import MotionTransformer
+from model.SimpleTransformer import SimpleTransformer
+from model.LSTM import SequenceModel
 import wandb
-from DataLoading.aug_utils import change_image_brightness_rgb, add_random_shadow
 # noinspection PyAttributeOutsideInit
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -44,16 +40,28 @@ class AverageMeter(object):
 device = torch.device("cuda")
 
 parameters = edict(
-    learning_rate = 0.001,
-    batch_size = 64,
+    learning_rate = 0.0001,
+    batch_size = 13,
     seq_len = 5,
     num_workers = 8,
+    model_name = 'SimpleTransformer',
+    normalization = ([0.485, 0.456, 0.406],
+                    [0.229, 0.224, 0.225]),
+    image_size=(224, 224),
+    epochs=161,
+    all_frames=False,
+    optical_flow=False
 )
-
-
-network = Convolution3D()
-#network = torch.nn.DataParallel(network).to(device)
-#network.load_state_dict(torch.load('saved_models/CNN3D/epoch-31.tar'))
+if parameters.model_name == 'LSTM':
+    model_object = SequenceModel
+elif parameters.model_name == 'MotionTransformer' :
+    model_object = MotionTransformer
+elif parameters.model_name == 'SimpleTransformer' :
+    model_object = SimpleTransformer
+else:
+    raise KeyError("Unknown Architecture")
+    
+network = model_object(parameters.seq_len) 
 network.to(device)
 wandb.init(config=parameters, project='self-driving-car')
 wandb.watch(network)
@@ -71,30 +79,38 @@ split_point = int(dataset_size * 0.9)
 training_set = UD.UdacityDataset(csv_file='/home/chingis/self-driving-car/output/interpolated.csv',
                              root_dir='/home/chingis/self-driving-car/output/',
                              transform=transforms.Compose([
-                                 transforms.Resize((120,320)),
+                                 #transforms.Resize((224,224)),#(120,320)
                                  transforms.ToTensor(),
-                                 transforms.Normalize(0.5, 0.5)
+                                 transforms.Normalize(*parameters.normalization)
                                  ]),
+                             img_size=parameters.image_size,
+                             seq_len=parameters.seq_len,
+                             optical_flow=parameters.optical_flow,
                              select_camera='center_camera',
                              select_range=(0,split_point))
 validation_set = UD.UdacityDataset(csv_file='/home/chingis/self-driving-car/output/interpolated.csv',
                              root_dir='/home/chingis/self-driving-car/output/',
                              transform=transforms.Compose([
-                                 transforms.Resize((120,320)),
+                                # transforms.Resize((224,224)),
                                  transforms.ToTensor(),
-                                 transforms.Normalize(0.5, 0.5)]),
+                                 transforms.Normalize(*parameters.normalization)
+                                 ]),
+                             img_size=parameters.image_size,
+                             seq_len=parameters.seq_len,
+                             optical_flow=parameters.optical_flow,
                              select_camera='center_camera',
                              select_range=(split_point,dataset_size))
 
 
-training_cbs = CB.ConsecutiveBatchSampler(data_source=training_set, batch_size=parameters.batch_size, shuffle=False, drop_last=False, seq_len=parameters.seq_len)
+training_cbs = CB.ConsecutiveBatchSampler(data_source=training_set, batch_size=parameters.batch_size,use_all_frames=parameters.all_frames, shuffle=True, drop_last=False, seq_len=parameters.seq_len)
 training_loader = DataLoader(training_set, sampler=training_cbs, num_workers=parameters.num_workers, collate_fn=(lambda x: x[0]))
 
-validation_cbs = CB.ConsecutiveBatchSampler(data_source=validation_set, batch_size=parameters.batch_size, shuffle=False, drop_last=False, seq_len=parameters.seq_len)
+validation_cbs = CB.ConsecutiveBatchSampler(data_source=validation_set, batch_size=parameters.batch_size, use_all_frames=False, shuffle=False, drop_last=False, seq_len=parameters.seq_len)
 validation_loader = DataLoader(validation_set, sampler=validation_cbs, num_workers=parameters.num_workers, collate_fn=(lambda x: x[0]))
-criterion =  torch.nn.MSELoss()
+criterion = torch.nn.MSELoss()
 criterion.to(device)
-scaler = torch.cuda.amp.GradScaler()
+speed_criterion =  torch.nn.SmoothL1Loss()
+speed_criterion.to(device)
 
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
@@ -106,53 +122,91 @@ def adjust_learning_rate(optimizer, epoch):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-for epoch in range(200):
-    losses = AverageMeter()
-    torch.save(network.state_dict(), "saved_models/CNN3D/epoch-{}.tar".format(epoch))
+for epoch in range(parameters.epochs):
+    train_angle_losses = AverageMeter()
+    train_speed_losses = AverageMeter()
+    torch.save(network.state_dict(), "saved_models/transformer/epoch.tar")
     network.train()
     adjust_learning_rate(optimizer, epoch)
     # Calculation on Training Loss
     for training_sample in tqdm(training_loader):
         param_values = [v for v in training_sample.values()]
-        image, angle, optical = param_values
+        if parameters.optical_flow:
+            image, angle, optical, speed = param_values
+            optical = optical.to(device)
+            speed = speed.float().reshape(-1, 1).to(device)
+        else:
+            image, angle = param_values
         cur_bn = image.shape[0]
-        image = image.permute(0,2,1,3,4)
-        optical = optical.permute(0,2,1,3,4)
+        #image = image.permute(0,2,1,3,4)
+        #optical = optical.permute(0,2,1,3,4)
 
-        
+        loss = 0
         image = image.to(device)
-        optical = optical.to(device)
-        with torch.cuda.amp.autocast():
-            prediction = network(image, optical)
-            prediction = prediction.reshape(-1, 1)
-            labels = angle.float().reshape(-1, 1).to(device)
-            training_loss_angle = torch.sqrt(criterion(prediction,labels))
-        losses.update(training_loss_angle.item())
+        #optical = optical.to(device)
+        #with torch.cuda.amp.autocast():
+        if parameters.optical_flow:
+            angle_hat, speed_hat = network(image, optical)  
+            speed_hat = speed_hat.reshape(-1, 1)
+            training_loss_speed = speed_criterion(speed_hat,speed)
+            loss += training_loss_speed
+            train_speed_losses.update(training_loss_speed.item())
+        else:
+            angle_hat = network(image)
+        #print(prediction.shape, angle.squeeze()[: , -angle.shape[1]:].shape)
+        angle_hat = angle_hat.reshape(-1, 1)
+        angle = angle.float().reshape(-1, 1).to(device)
+        training_loss_angle = torch.sqrt(criterion(angle_hat,angle) + 1e-6)
+        loss += training_loss_angle
+
+        train_angle_losses.update(training_loss_angle.item())
         optimizer.zero_grad()
-        scaler.scale(training_loss_angle).backward()
-        scaler.step(optimizer)
-        scaler.update()
+        loss.backward()
+        optimizer.step()
 
     
     print("Done")
-    network.eval()
-    # Calculation on Validation Loss
-    val_losses = AverageMeter()
-    with torch.no_grad():    
-        for Validation_sample in tqdm(validation_loader):
-            param_values = [v for v in Validation_sample.values()]
-            image, angle, optical = param_values
-            cur_bn = image.shape[0]
-            image = image.permute(0,2,1,3,4)
-            optical = optical.permute(0,2,1,3,4)
-            image = image.to(device)
-            optical = optical.to(device)
-            with torch.cuda.amp.autocast():
-                prediction = network(image, optical)
-                prediction = prediction.reshape(-1,1)
-                labels = angle.float().reshape(-1,1).to(device)
+    report = {}
+    if split_point != dataset_size:
+        network.eval()
+        val_speed_losses = AverageMeter()
+        val_angle_losses = AverageMeter()
+        with torch.no_grad():    
+            for Validation_sample in tqdm(validation_loader):
+                param_values = [v for v in Validation_sample.values()]
+                if parameters.optical_flow:
+                    image, angle, optical, speed = param_values
+                    optical = optical.to(device)
+                    speed = speed.float().reshape(-1, 1).to(device)
+                else:
+                    image, angle = param_values
+                cur_bn = image.shape[0]
+                #image = image.permute(0,2,1,3,4)
+                #optical = optical.permute(0,2,1,3,4)
+                loss = 0
+                image = image.to(device)
+                #optical = optical.to(device)
+                if parameters.optical_flow:
+                    angle_hat, speed_hat = network(image, optical)  
+                    speed_hat = speed_hat.reshape(-1, 1)
+                    validation_loss_speed = speed_criterion(speed_hat,speed)
+                    loss += validation_loss_speed
+                    val_speed_losses.update(validation_loss_speed.item())
+                else:
+                    angle_hat = network(image)
+                angle_hat = angle_hat.reshape(-1, 1)
+                angle = angle.float().reshape(-1, 1).to(device)
+    
+                validation_loss_angle = torch.sqrt(criterion(angle_hat,angle) + 1e-6)
+                loss += validation_loss_angle
+    
+                val_angle_losses.update(validation_loss_angle.item())
+        report['val_loss'] = val_angle_losses.avg
+    report['training_loss'] = train_angle_losses.avg
+    if parameters.optical_flow:
+        report['training_speed_loss'] = train_speed_losses.avg
+        if split_point != dataset_size:
+            report['validation_speed_loss'] = val_speed_losses.avg    
 
-                validation_loss_angle = torch.sqrt(criterion(prediction,labels))
-            val_losses.update(validation_loss_angle.item())
-    wandb.log({'training_loss': losses.avg, 'val_loss': val_losses.avg})
+    wandb.log(report)
     
