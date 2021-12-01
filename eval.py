f569d7b4a8d9c843bb9e4b39413b452b1fbe101e
Chingis Oinar <chingisoinar@gmail.com>
2021-12-01 16:14:17 +0900

    Add files via upload

--- eval.py
+++ eval.py
@@ -21,12 +21,13 @@ import torch.backends.cudnn as cudnn
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from model.TransMotion import Convolution3D
+from model.MotionTransformer import MotionTransformer
+from model.LSTM import SequenceModel
 from DataLoading import EvalDataset as ED
 from DataLoading import ConsecutiveBatchSampler as CB
 import json
 import numpy as np
-
+from model.SimpleTransformer import SimpleTransformer
 #import wandb
 # noinspection PyAttributeOutsideInit
 class AverageMeter(object):
@@ -49,64 +50,95 @@ class AverageMeter(object):
 device = torch.device("cuda")
 
 parameters = edict(
-    file = '3DCNN_Paper', # used to mark specific files in case that we want to check them on tensorboard
-    batch_size = 64,
+    learning_rate = 0.001,
+    batch_size = 13,
     seq_len = 5,
-    num_workers = 8,
+    num_workers = 16,
+    model_name = 'SimpleTransformer',
+    normalization = ([0.485, 0.456, 0.406],
+                    [0.229, 0.224, 0.225]),
+    image_size=(224, 224),
+    epochs=161,
+    all_frames=True,
+    optical_flow=False,
+    checkpoint='saved_models/transformer/simpletransformer.tar'
 )
 
 
-network = Convolution3D()
-network.load_state_dict(torch.load('saved_models/CNN3D/epoch-31.tar'))
+if parameters.model_name == 'LSTM':
+    model_object = SequenceModel
+elif parameters.model_name == 'MotionTransformer' :
+    model_object = MotionTransformer
+elif parameters.model_name == 'SimpleTransformer' :
+    model_object = SimpleTransformer
+else:
+    raise KeyError("Unknown Architecture")
+    
+network = model_object(parameters.seq_len)  
+network.load_state_dict(torch.load(parameters.checkpoint))
 network.to(device)
 #wandb.init(config=parameters, project='self-driving-car')
 #wandb.watch(network)
 
 validation_set = ED.EvalDataset(csv_file='/home/chingis/self-driving-car/CH2_final_evaluation.csv',
                              root_dir='/home/chingis/self-driving-car/center/',
-                             transform=transforms.Compose([              
-                                 transforms.Lambda(lambda x: Image.fromarray(np.uint8(x)).convert('RGB')),
-                                 transforms.Resize((120,320)),
+                             transform=transforms.Compose([
+                                # transforms.Resize((224,224)),
                                  transforms.ToTensor(),
-                                 transforms.Normalize(0.5, 0.5)]), public=False, private=False)
+                                 transforms.Normalize(*parameters.normalization)
+                                 ]),
+                             img_size=parameters.image_size,
+                             optical_flow=parameters.optical_flow,
+                             )
 
 
-validation_cbs = CB.ConsecutiveBatchSampler(data_source=validation_set, batch_size=parameters.batch_size, shuffle=False, drop_last=False, seq_len=parameters.seq_len)
+
+validation_cbs = CB.ConsecutiveBatchSampler(data_source=validation_set, batch_size=parameters.batch_size, shuffle=False, drop_last=False, seq_len=parameters.seq_len, use_all_frames=parameters.all_frames)
 validation_loader = DataLoader(validation_set, sampler=validation_cbs, num_workers=parameters.num_workers, collate_fn=(lambda x: x[0]))
 criterion =  torch.nn.MSELoss()
 criterion.to(device)
-scaler = torch.cuda.amp.GradScaler()
 
-network.eval()
 predictions = []
-# Calculation on Validation Loss
-val_losses = AverageMeter()
+network.eval()
+val_angle_losses = AverageMeter()
 with torch.no_grad():    
     for Validation_sample in tqdm(validation_loader):
         param_values = [v for v in Validation_sample.values()]
-        image, angle,idx, optical = param_values
+        if parameters.optical_flow:
+            image, angle, idx,  optical = param_values
+            optical = optical.to(device)
+        else:
+            image, angle, idx = param_values
         cur_bn = image.shape[0]
-        image = image.permute(0,2,1,3,4)
-        optical = optical.permute(0,2,1,3,4)
+        #image = image.permute(0,2,1,3,4)
+        #optical = optical.permute(0,2,1,3,4)
+        loss = 0
         image = image.to(device)
-        optical = optical.to(device)
-        with torch.cuda.amp.autocast():
-            prediction = network(image, optical)
-            prediction = prediction.reshape(-1,1)
-            labels = angle.float().reshape(-1,1).to(device)
+        #optical = optical.to(device)
+        if parameters.optical_flow:
+            angle_hat, _ = network(image, optical)  
 
-            validation_loss_angle = criterion(prediction,labels)
-        val_losses.update(validation_loss_angle.item())
-        predictions.extend(list(zip(idx.detach().cpu().flatten().numpy().tolist(), prediction.detach().cpu().flatten().numpy().tolist() )))
+        else:
+            angle_hat = network(image)
+        angle_hat = angle_hat.reshape(-1, 1)
+        angle = angle.float().reshape(-1, 1).to(device)
+   
+        validation_loss_angle = torch.sqrt(criterion(angle_hat,angle) + 1e-6)
+        loss += validation_loss_angle
+   
+        val_angle_losses.update(validation_loss_angle.item())
+        predictions.extend(list(zip(idx.detach().cpu().flatten().numpy().tolist(), angle_hat.detach().cpu().flatten().numpy().tolist() )))
 
 
-print(val_losses.avg)
+print(val_angle_losses.avg)
 
 res_dic = {} 
 for pair in predictions:
     idx, angle = pair
     if idx not in res_dic.keys():
         res_dic[idx] = angle
+    else:
+        res_dic[idx] = res_dic[idx] * 0.5 + 0.5 * angle
 
 
 a_file = open("predictions.json", "w")
